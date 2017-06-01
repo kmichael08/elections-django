682f80ca637a09181e8b6d3bdb80ad3a4ce0cd01
Michał Kuźba <kmichael00968@gmail.com>
2017-06-01 20:02:40 +0200

    urls polish characters fix

--- president/urls.py
+++ president/urls.py
@@ -11,24 +11,24 @@ urlpatterns = [
     url(r'^logout/', views.logout_view, name='logout_view'),
 
     # ex: /polska/obwód/2608053/upload_pdf
-    url(r'^obwód/(?P<name>[ 0-9A-Zążęćńśół_a-z-]+)/upload_pdf/$', views.upload_pdf, name='upload_pdf'),
+    url(r'^obwód/(?P<name>[ 0-9A-ZążęćńśółŁĆŚÓŻ_a-z-]+)/upload_pdf/$', views.upload_pdf, name='upload_pdf'),
 
 
     url(r'^search/$', views.search, name='search'),
 
     # ex: /polska/edit_votes/2608053
-    url(r'^edit_votes_dynamic/(?P<name>[ 0-9A-Zążęćńśół_a-z-]+)/$', views.edit_votes_dynamic, name='edit_votes_dynamic'),
+    url(r'^edit_votes_dynamic/(?P<name>[ 0-9A-ZążęćńśółŁĆŚÓŻ_a-z-]+)/$', views.edit_votes_dynamic, name='edit_votes_dynamic'),
 
     url(r'^lista_gmin/$', views.lista_gmin, name='lista_gmin'),
 
     # ex: /polska/media/obwód_2608053
-    url(r'^media/(?P<filename>[ _ążźśęćńłó0-9A-Za-z-]+)/$', views.get_pdf, name='get_pdf'),
+    url(r'^media/(?P<filename>[ _ążźśęćńłóŁĆŚÓŻ0-9A-Za-z-]+)/$', views.get_pdf, name='get_pdf'),
 
     # ex: /polska/powiat/nakielski
-    url(r'^(?P<typ>[ 0-9A-Zążęćńśół_a-z-]+)/(?P<name>[ _ążźśęćńłó0-9A-Za-z-]+)/$', views.get_unit, name='get_unit'),
+    url(r'^(?P<typ>[ 0-9A-Zążęćńśół_a-z-]+)/(?P<name>[ _ążźśęćńłóŁĆÓŚŻ0-9A-Za-z-]+)/$', views.get_unit, name='get_unit'),
 
     # ex: /polska/data/powiat/nakielski
-    url(r'^data/(?P<typ>[ 0-9A-Zążęćńśół_a-z-]+)/(?P<name>[ _ążźśęćńłó0-9A-Za-z-]+)/$', views.get_unit_data, name='get_unit_data'),
+    url(r'^data/(?P<typ>[ 0-9A-Zążęćńśół_a-z-]+)/(?P<name>[ _ążźśęćńłóŁĆŚÓŻ0-9A-Za-z-]+)/$', views.get_unit_data, name='get_unit_data'),
 
 
 ]
