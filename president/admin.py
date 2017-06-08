9cc3856dbc48f68201e0762199b138c3c9da3012
Michał Kuźba <kmichael00968@gmail.com>
2017-06-08 23:45:53 +0200

    Django admin - final commit

--- president/admin.py
+++ president/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Result, Unit
-
+from .models import Result, Unit, Candidate
 
 class ResultAdmin(admin.ModelAdmin):
     list_display = ('id_cand', 'typ', 'name')
@@ -10,21 +9,43 @@ class ResultAdmin(admin.ModelAdmin):
         return obj.id_unit.type
     def name(self, obj):
         return obj.id_unit.name
-    list_filter = ['id_cand']
+    list_filter = ['id_cand', 'id_unit__type']
     search_fields = ['id_unit__type', 'id_unit__name']
-
-    def get_queryset(self, request):
-        qs = super(ResultAdmin, self).get_queryset(request)
-        return qs.filter(id_unit__type='obwód')
 
 admin.site.register(Result, ResultAdmin)
 
 class UnitAdmin(admin.ModelAdmin):
-    list_display = ('type', 'name', 'result_file')
+    list_display = ('type', 'name', 'short_name')
     search_fields = ['name']
+    list_filter = ['type']
+    fields = ('type', 'name', 'short_name')
 
-    def get_queryset(self, request):
-        qs = super(UnitAdmin, self).get_queryset(request)
-        return qs.filter(type='obwód')
 
 admin.site.register(Unit, UnitAdmin)
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'name', 'surname', 'second_name')
+    search_fields = ['name', 'surname', 'second_name']
+
+admin.site.register(Candidate, CandidateAdmin)
+
+
+class Obwod(Unit):
+    class Meta:
+        proxy = True
+
+    objects = Unit()
+
+
+class PdfAdmin(admin.ModelAdmin):
+    list_display = ('type', 'name', 'result_file')
+    search_fields = ['name']
+    list_filter = ['type']
+    list_display_links = ('name',)
+
+    def get_queryset(self, request):
+        qs = super(PdfAdmin, self).get_queryset(request)
+        return qs.filter(type = "obwód")
+
+
+admin.site.register(Obwod, PdfAdmin)
